e1c17435043a1d0c7d452963badcc7c45495da76
Brent Piephoff <brent.piephoff@gmail.com>
2022-02-07 13:56:36 -0800

    debugging crash related to relationships object

--- tap_outreach/sync.py
+++ tap_outreach/sync.py
@@ -216,7 +216,7 @@ def process_records(stream, mdata, max_modified, records, filter_field, fks):
             if 'relationships' in record:
                 for prop, value in record['relationships'].items():
                     if 'data' not in value and 'links' not in value:
-                        if value == True
+                        if value == True:
                             raise Exception(
                                 'Only `data` or `links` expected in relationships, value is {}'.format(value))
 
